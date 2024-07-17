4d0197a5ffe4083b9520536c0f3dd5ef4eb2b0a8
Shanmugalakshmi11 <shanmugalakshmi.chandrasekaran@docc.techstarter.de>
2024-07-17 17:14:16 +0200

    PORT Changes

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use('/api', userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
