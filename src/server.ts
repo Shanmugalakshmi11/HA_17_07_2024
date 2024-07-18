4098579f387ed5e7946e800c1476937ec9073f13
Shanmugalakshmi11 <shanmugalakshmi.chandrasekaran@docc.techstarter.de>
2024-07-18 09:35:49 +0200

    server updated

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import AppRouter from './routes';
+import userRoutes from './routes/UserRoutes';
 
 // Initialisierung von expres
 const server = express();
@@ -9,5 +10,6 @@ server.use(bodyParser.json());
 // Use for development
 server.use(cors());
 server.use('/v1', AppRouter);
+server.use('/api', userRoutes);
 
 export default server;
