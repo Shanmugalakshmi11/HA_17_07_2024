82be4534ba0d3e46a46c9f6393906c6f4f4b4ab7
Shanmugalakshmi11 <shanmugalakshmi.chandrasekaran@docc.techstarter.de>
2024-07-18 10:08:41 +0200

    commit

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -3,6 +3,8 @@ import { User } from '../interfaces/UserInterface';
 const users: User[] = [
   { id: '1', name: 'John Doe', email: 'john@example.com' },
   { id: '2', name: 'Jane Smith', email: 'jane@example.com' },
+  { id: '3', name: 'Benjamin Müller ', email: 'benjamin@example.com' },
+  { id: '4', name: 'Anit Domica', email: 'anit@example.com' },
   // Add more mock users as necessary
 ];
 
